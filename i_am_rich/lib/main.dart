b360016bae5b8b8c1dd248d7ec28c8f4f9d7ce13
NatnaelTaddese <natnaeltaddese3@gmail.com>
2023-10-06 14:11:41 -0700

    fetching images from network

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,10 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Center(
-      child: Text("Am I tho?"),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text("I am Rich"),
+        ),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: const Center(
+        child: Image(
+          image: NetworkImage(
+              'https://upload.wikimedia.org/wikipedia/commons/0/00/Diamond_4.jpg'),
+        ),
+      ),
+      backgroundColor: Colors.blueGrey[300],
     ),
   ));
 }
